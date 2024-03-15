8fcc952477515d2c75d774d0ea6ec1aaa917a4c7
Thiago Barros <bunbismuth@proton.me>
2024-03-15 20:22:54 -0300

    build: project build

---
+++ picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/PicpayDesafioBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.bunbismuth.picpaydesafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+	}
+
+}
