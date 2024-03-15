5369e5c2ebee4492bd1acce392217d8fa6f88c2c
Thiago Barros <bunbismuth@proton.me>
2024-03-15 20:23:29 -0300

    feat: create Transaction table

---
+++ picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/transaction/Transaction.java
@@ -0,0 +1,19 @@
+package br.com.bunbismuth.picpaydesafiobackend.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("TRANSACTIONS")
+public record Transaction (
+  @Id Long id,
+  Long payer,
+  Long payee,
+  BigDecimal value,
+  @CreatedDate LocalDateTime createdAt) {
+  public Transaction {
+      value = value.setScale(2);
+    }
+}
