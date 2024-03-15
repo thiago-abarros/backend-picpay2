d8f00c50e7000ce387f4a55fde2dbd0bf7ef9692
Thiago Barros <bunbismuth@proton.me>
2024-03-15 20:24:40 -0300

    feat: create ExceptionHandler

---
+++ picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/transaction/TransactionExceptionHandler.java
@@ -0,0 +1,14 @@
+package br.com.bunbismuth.picpaydesafiobackend.transaction;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class TransactionExceptionHandler {
+
+  @ExceptionHandler(InvalidTransactionException.class)
+  public ResponseEntity<Object> handle(InvalidTransactionException exception) {
+    return ResponseEntity.badRequest().body(exception.getMessage());
+  }
+}
