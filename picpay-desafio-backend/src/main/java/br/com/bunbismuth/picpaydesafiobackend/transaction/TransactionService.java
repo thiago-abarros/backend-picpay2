b15d7186d65cab7e46f1b9211e466654552b997d
Thiago Barros <bunbismuth@proton.me>
2024-03-16 14:28:37 -0300

    refactor: added authorization and notification

--- picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/transaction/TransactionService.java
+++ picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/transaction/TransactionService.java
@@ -1,5 +1,7 @@
 package br.com.bunbismuth.picpaydesafiobackend.transaction;
 
+import br.com.bunbismuth.picpaydesafiobackend.authorization.AuthorizerService;
+import br.com.bunbismuth.picpaydesafiobackend.notification.NotificationService;
 import br.com.bunbismuth.picpaydesafiobackend.wallet.WalletRepository;
 import br.com.bunbismuth.picpaydesafiobackend.wallet.WalletType;
 import java.util.List;
@@ -10,14 +12,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TransactionService {
+
   private static final Logger LOGGER = LoggerFactory.getLogger(TransactionService.class);
   private final TransactionRepository transactionRepository;
   private final WalletRepository walletRepository;
+  private final NotificationService notificationService;
+  private final AuthorizerService authorizerService;
 
   public TransactionService(TransactionRepository transactionRepository,
-      WalletRepository walletRepository) {
+      WalletRepository walletRepository, NotificationService notificationService,
+      AuthorizerService authorizerService) {
     this.transactionRepository = transactionRepository;
     this.walletRepository = walletRepository;
+    this.notificationService = notificationService;
+    this.authorizerService = authorizerService;
   }
 
   @Transactional
@@ -27,15 +35,14 @@ public class TransactionService {
     var newTransaction = transactionRepository.save(transaction);
 
     var walletPayer = walletRepository.findById(transaction.payer()).get();
-    var wallerPayee = walletRepository.findById(transaction.payee()).get();
+    var walletPayee = walletRepository.findById(transaction.payee()).get();
     walletRepository.save(walletPayer.debit(transaction.value()));
-    walletRepository.save(wallerPayee.credit(transaction.value()));
+    walletRepository.save(walletPayee.credit(transaction.value()));
+
+    authorizerService.authorize(transaction);
+    notificationService.notify(newTransaction);
 
     return newTransaction;
-  }
-
-  public List<Transaction> list() {
-    return transactionRepository.findAll();
   }
 
   /*
@@ -49,12 +56,18 @@ public class TransactionService {
 
     walletRepository.findById(transaction.payee())
         .map(payee -> walletRepository.findById(transaction.payer())
-          .map(
-              payer -> payer.type() == WalletType.COMUM.getValue() &&
-                  payer.balance().compareTo(transaction.value()) >= 0 &&
-                  !payer.id().equals(transaction.payee()) ? true : null)
-          .orElseThrow(() -> new InvalidTransactionException(
-            "Invalid transaction - " + transaction
-        ))).orElseThrow(() -> new InvalidTransactionException("Invalid transaction - " + transaction));
+            .map(
+                payer -> payer.type() == WalletType.COMUM.getValue() &&
+                    payer.balance().compareTo(transaction.value()) >= 0 &&
+                    !payer.id().equals(transaction.payee()) ? true : null)
+            .orElseThrow(() -> new InvalidTransactionException(
+                "Invalid transaction - " + transaction)))
+        .orElseThrow(() -> new InvalidTransactionException(
+            "Invalid transaction - " + transaction));
   }
+
+  public List<Transaction> list() {
+    return transactionRepository.findAll();
+  }
+
 }
