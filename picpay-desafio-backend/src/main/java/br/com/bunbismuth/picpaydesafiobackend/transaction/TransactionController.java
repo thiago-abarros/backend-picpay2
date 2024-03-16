ad29866411083ca3d916b7ef69800e390b252411
Thiago Barros <bunbismuth@proton.me>
2024-03-16 11:34:38 -0300

    feat: create route Post Transaction

--- picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/transaction/TransactionController.java
+++ picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/transaction/TransactionController.java
@@ -2,6 +2,8 @@ package br.com.bunbismuth.picpaydesafiobackend.transaction;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,11 @@ public class TransactionController {
 
   public TransactionController(TransactionService transactionService) {
     this.transactionService = transactionService;
+  }
+
+  @PostMapping
+  public Transaction createTransaction(@RequestBody Transaction transaction) {
+    return transactionService.create(transaction);
   }
 
   @GetMapping
--- picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/transaction/TransactionService.java
+++ picpay-desafio-backend/src/main/java/br/com/bunbismuth/picpaydesafiobackend/transaction/TransactionService.java
@@ -1,21 +1,38 @@
 package br.com.bunbismuth.picpaydesafiobackend.transaction;
 
+import br.com.bunbismuth.picpaydesafiobackend.wallet.WalletRepository;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TransactionService {
   private static final Logger LOGGER = LoggerFactory.getLogger(TransactionService.class);
   private final TransactionRepository transactionRepository;
+  private final WalletRepository walletRepository;
 
-  public TransactionService(TransactionRepository transactionRepository) {
+  public TransactionService(TransactionRepository transactionRepository,
+      WalletRepository walletRepository) {
     this.transactionRepository = transactionRepository;
+    this.walletRepository = walletRepository;
+  }
+
+  @Transactional
+  public Transaction create(Transaction transaction) {
+
+    var newTransaction = transactionRepository.save(transaction);
+
+    var walletPayer = walletRepository.findById(transaction.payer()).get();
+    var wallerPayee = walletRepository.findById(transaction.payee()).get();
+    walletRepository.save(walletPayer.debit(transaction.value()));
+    walletRepository.save(wallerPayee.credit(transaction.value()));
+
+    return newTransaction;
   }
 
   public List<Transaction> list() {
     return transactionRepository.findAll();
   }
-
 }
